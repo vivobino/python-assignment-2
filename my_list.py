f41c9546a604657b2f76a81fdb3980474da4f2ea
vivian obino <158448909+vivobino@users.noreply.github.com>
2024-02-29 18:36:23 +0300

    Add files via upload

---
+++ my_list.py
@@ -0,0 +1,23 @@
+my_list=[]
+print(my_list)
+my_list.append(10)
+print("After Append:",my_list)
+my_list.append(20)
+print("After Append:",my_list)
+my_list.append(30)
+print("After Append:",my_list)
+my_list.append(40)
+print("After Append:",my_list)
+my_list[1]=15
+print(my_list)
+list= [50,60,70]
+my_list.extend(list)
+print("list after append:",my_list)
+my_list.remove(70)
+print(my_list)
+my_list.sort
+print(my_list)
+
+
+
+
